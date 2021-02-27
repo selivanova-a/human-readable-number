3ae7df43e57f6f4dd3594564429c5f297bf1fc2b
Anastasiia Selivanova <selivanovaana5tacia@yandex.ru>
2021-02-27 13:59:42 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
-}
+  const unitsArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const tenPlusArray = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tensArray = [null, 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  const hundreds = Math.floor(number / 100);
+  const tens = Math.floor((number - (hundreds * 100)) / 10);
+  const units = number - (hundreds * 100) - (tens * 10);
+
+  let answer = [];
+  if (number === 0) answer.push(`${unitsArray[0]}`);
+  if (hundreds > 0) answer.push(`${unitsArray[hundreds]} hundred`);
+  if (tens === 1) answer.push(`${tenPlusArray[units]}`);
+  if (tens > 1) answer.push(`${tensArray[tens]}`);
+  if (units > 0 && tens !== 1) answer.push(`${unitsArray[units]}`);
+
+  return answer.join(' ');
+};
